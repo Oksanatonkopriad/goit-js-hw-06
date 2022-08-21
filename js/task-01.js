a42a751fbad9eab468aca57248fc2d273e1f784c
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-08-21 12:34:16 +0300

    task2

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,7 @@
-const d
+const categoriesEl = document.querySelectorAll(`.item`);
+console.log(`Number of categories:`, categoriesEl.length);
+
+categoriesEl.forEach(function (item) {
+  console.log(`Category:`, item.firstElementChild.textContent);
+  console.log(`Elements:`, item.lastElementChild.children.length);
+});
