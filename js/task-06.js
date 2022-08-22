5921fcf7d90c852f1fd2e5de069e6419189fb35d
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-08-22 23:07:42 +0300

    task8

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector(`#validation-input`);
 function onInput(name) {
   const inpLength = Number(input.getAttribute(`data-length`));
 
-  if (name.currentTarget.value.length <= inpLength) {
+  if (name.currentTarget.value.length === inpLength) {
     input.classList.add("valid");
     input.classList.remove("invalid");
   } else {
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email;
+  const password = formElements.password;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены");
+  }
+  console.log({ email: email.value, password: password.value });
+
+  form.reset();
+}
