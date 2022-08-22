a0363a4a59aa2321d1ebf22fbca32e727e19860e
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-08-22 20:13:21 +0300

    task6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector(`#validation-input`);
+
+function onInput(name) {
+  const inpLength = Number(input.getAttribute(`data-length`));
+
+  if (name.currentTarget.value.length <= inpLength) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  }
+}
+
+input.addEventListener(`blur`, onInput);
