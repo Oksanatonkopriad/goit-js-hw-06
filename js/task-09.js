a6c21f34061bdfa4ec99fb407071446e03137076
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-08-28 13:20:33 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const bodyEl = document.querySelector(`body`);
+const spanEl = document.querySelector(`.color`);
+const btnEl = document.querySelector(`.change-color`);
+
+btnEl.addEventListener(`click`, bodyColor);
+
+function bodyColor() {
+  document.body.setAttribute(
+    "style",
+    "background-color: " + spanEl.textContent
+  );
+  spanEl.textContent = getRandomHexColor();
 }
